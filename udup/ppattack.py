79f99d369280ba11734e3912a960382dfad94c81
1462492739@qq,com <1462492739@qq.com>
2023-11-16 21:20:40 +0800

    djc
    djc

--- udup/ppattack.py
+++ udup/ppattack.py
@@ -1,5 +1,5 @@
 import os
-
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 from UDUP_pp.udup.udup_pp import UDUPpp_Attack
 import UDUP_pp.Allconfig.Path_Config as pconfig
 
@@ -11,7 +11,7 @@ udup_pp=UDUPpp_Attack(
     save_dir=pconfig.eval_demo_save_path,save_gt_res=pconfig.eval_demo_gt_path,
     det_model=None,rec_model='crnn',device_name='cuda:0',
     miss_or_show='miss',
-    adv_patch_size=(1,3,20,20),
-    eps=50,alpha=3,decay=0.0,T=100,
+    adv_patch_size=(1,3,30,30),
+    eps=40,alpha=3,decay=1.0,T=100,
     batch_size=20,batch_gt_num=10,gap=20, lambda_rec=1.0)
 udup_pp.train()
